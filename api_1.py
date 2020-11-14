d02a3d77d6c6c18f45c5083b5eeeec5c4f90c268
yassataiseer <1taiseeryas@hdsb.ca>
2020-11-14 17:16:37 -0500

    commit on main

--- api_1.py
+++ api_1.py
@@ -2,17 +2,19 @@ from flask import Flask, jsonify
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
-
 from order_classes import order_writer
+from clients_class import client
 conn = sqlite3.connect('orders.db', check_same_thread=False)
 c = conn.cursor()
 app = Flask(__name__)
 api = Api(app)
 c = conn.cursor()
+
+""" order.db classes for api """
 class Order_builder(Resource):
     def get(self,order_data):
         order_writer.data_entry(order_data)
-        return "yay"
+        return "Done!"
 
 class Order_finder(Resource):
     def get(self,order_number):
@@ -32,13 +34,27 @@ class Last_order(Resource):
     def get(self):
         a =order_writer.ticket_builder()
         return a  
+""" clients.db classes for api """
+class Client_data(Resource):
+    def get(self):
+        a = client.data()
+        return jsonify(a)
+
+class Specific_Client_Data(Resource):
+    def get(self,name):
+        a =  client.client_data(name)
+        return jsonify(a)
+class Client_Name(Resource):
+    def get(self,name):
+        pass
 
 api.add_resource(Order_builder,"/order_builder/<string:order_data>")
 api.add_resource(Order_Editor,"/order_editor/<string:change_data>")
 api.add_resource(Order_finder,"/Order_finder/<string:order_number>")
 api.add_resource(All_orders,"/All_orders")
 api.add_resource(Last_order,"/Last_order")
-
+api.add_resource(Client_data,"/Client_data")
+api.add_resource(Specific_Client_Data,"/Specific_Client_Data/<string:name>")
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- clients_class.py
+++ clients_class.py
@@ -8,34 +8,38 @@ c = conn.cursor()
 
 
 class client:
-        def data():
+    def data():
+        points = ["Name", "Adress1","Adress2","postalcode","email","phone"]
         send_data=[]
         c.execute('SELECT * FROM stuffToPlot')
         data = c.fetchall()
         #print(data)
         send_data = []
         for rows in data:
-            send_data.append(rows)
-        return( send_data)
+            final = dict(zip(points,rows))
+            send_data.append(final)
+        return send_data
     def name_data():#gets name of person
+        points = ["Name", "Adress1","Adress2","postalcode","email","phone"]
         send_data=[]
         c.execute('SELECT name FROM stuffToPlot')
         data = c.fetchall()
         #print(data)
         send_data = []
         for rows in data:
-            send_data.append(rows)
+            final = dict(zip(points,rows))
+            send_data.append(final)        
         return( send_data)
     def client_data(name):
+        points = ["Name", "Adress1","Adress2","postalcode","email","phone"]
         send_data=[]
         c.execute('SELECT * FROM stuffToPlot')
         data = c.fetchall()
         for rows in data:
             if name==rows[0]:
-                send_data.append(rows)
-            else:
-                pass
-        return(send_data)
+                final = dict(zip(points,rows))
+                send_data.append(final)
+        return send_data
     def clients_write(name,address1,address2,postcode,email,phone):
             c.execute("SELECT name FROM stuffToPlot")
             a = c.fetchall()
