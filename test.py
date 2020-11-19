2eba655d546aa6a1f54d3be5baa7754363e92557
yassataiseer <1taiseeryas@hdsb.ca>
2020-11-19 12:19:30 -0500

    commit on main

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ BASE = "http://127.0.0.1:5000/"
 string = "Bill Gates+fake st+None+l9E 1k5+test@gmail.com+1234567890"
 #string = string.split("+")
 #print(string)
-response = requests.get(BASE+"Add_client/"+string)
+response = requests.get('http://inkfactory.pythonanywhere.com/Last_order')
 #response = requests.get(BASE+"/All_orders")
 
 
-#print(response.json())
+print(response.json())
 
 
